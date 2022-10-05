bd22080a7c4cabbd79582e7b6b6795d604636c4e
Justin Mueller <muelljustin@gmail.com>
2022-10-05 17:27:48 +0200

    commit

--- train.py
+++ train.py
@@ -1,8 +1,9 @@
 import nltk
+from nltk.stem import WordNetLemmatizer
+
 nltk.download('punkt')
 nltk.download('wordnet')
 nltk.download('omw-1.4')
-from nltk.stem import WordNetLemmatizer
 lemmatizer = WordNetLemmatizer()
 import json
 import pickle
